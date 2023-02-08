ad76b6823e4e4008adbde9a7423d24aea8720185
muslimNerd <sysavanefanta49@gmail.com>
2023-02-08 10:58:41 +0100

    first commit

---
+++ api-project/src/UserList.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => setUsers(res.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
